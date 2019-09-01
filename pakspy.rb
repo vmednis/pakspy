8d632c719258fd2df971ee97d83e96c4ce6bcc4e
Valters Mednis <valters.mednis@gmail.com>
2019-09-01 18:46:43 +0300

    Added basic extraction

--- pakspy.rb
+++ pakspy.rb
@@ -1,42 +1,82 @@
-class PAKHeader
-  attr_accessor :magic, :offset, :size
+##
+# Class handles Quake style PAK files
+class PAKFile
+  attr_reader :path, :header, :file_entries
 
-  def initialize(header)
-    # magic  - 4 byte arbitary string (not null terminated)
-    # offset - 4 byte integer (little endian)
-    # size   - 4 byte integer (little endian)
-    data = header.unpack "a4VV"
+  ##
+  # Opens a pak file from +path+
+  def initialize(path)
+    @path = path
+    @file = File.open path, "rb"
 
-    @magic  = data[0]
-    @offset = data[1]
-    @size   = data[2]
+    @header = PAKHeader.new @file.read 12
+    file_count = @header.size / 64
+
+    @file_entries = Hash.new
+    @file.seek @header.offset
+    file_count.times do
+      entry = PAKFileEntry.new @file.read 64
+      @file_entries[entry.name] = entry
+      puts entry.name
+    end
+  end
+
+  def finalize
+    @file.close
+  end
+
+  ##
+  # Extracts a file +name+ from this PAKFile to +path+ on system
+  def extract(name, path)
+    raise ArgumentError, "No such file #{name} in this PAK." if @file_entries[name].nil?
+
+    output_file = File.new path, "wb"
+
+    @file.seek @file_entries[name].offset
+    output_file.write @file.read @file_entries[name].size
+
+    output_file.close
+  end
+
+  ##
+  # Reperesents PAK header block
+  class PAKHeader
+    attr_accessor :magic, :offset, :size
+
+    ##
+    # Unpacks the 12 header bytes from +entry+
+    def initialize(header)
+      # magic  - 4 byte arbitary string (not null terminated)
+      # offset - 4 byte integer (little endian)
+      # size   - 4 byte integer (little endian)
+      data = header.unpack "a4VV"
+
+      @magic  = data[0]
+      @offset = data[1]
+      @size   = data[2]
+    end
+  end
+
+  ##
+  # Represents PAK file entry block
+  class PAKFileEntry
+    attr_accessor :name, :offset, :size
+
+    ##
+    # Unpacks the 64 file entry bytes from +entry+
+    def initialize(entry)
+      # name   - max 58 byte null terminated string
+      # offset - 4 byte integer (little endian)
+      # size   - 4 byte integer (little endian)
+      data = entry.unpack "a56VV"
+
+      @name   = data[0].rstrip
+      @offset = data[1]
+      @size   = data[2]
+    end
   end
 end
 
-class PAKFileEntry
-  attr_accessor :name, :offset, :size
+file = PAKFile.new ARGV[0]
 
-  def initialize(entry)
-    # name   - max 58 byte null terminated string
-    # offset - 4 byte integer (little endian)
-    # size   - 4 byte integer (little endian)
-    
-    data = entry.unpack "a56VV"
-
-    @name   = data[0].rstrip
-    @offset = data[1]
-    @size   = data[2]
-  end
-end
-
-file = File.open ARGV[0], "rb"
-header = PAKHeader.new file.read 12
-file_count = header.size / 64
-puts "#{ARGV[0]} contains #{file_count} files:"
-
-file.seek header.offset
-file_count.times do 
-  file_entry = PAKFileEntry.new file.read 64
-  file_size_kib = (file_entry.size / 1024.0).round 2
-  puts "#{file_entry.name} #{file_size_kib} KiB"
-end
+file.extract(ARGV[1], ARGV[2]) if ARGV.size == 3
